1655ce53385c59df8359f9ab7728ae2d4507c16e
Sava Tshontikidis <stshontikidis@discoveryed.com>
2020-12-28 09:48:25 -0500

    Use single quotes around var
    So I guess when doing [[ -z $foo ]] if foo is empty then its [[ -z ]]
    and the shell does not know the condtion, surronding it in single quotes
    makes it [[ -z '' ]] if empty so it actual tests correctly

--- scripts/work_tmux.sh
+++ scripts/work_tmux.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+vpn_up=$(ifconfig | grep 172.16)
 session='Work'
 
 cd $HOME
@@ -8,7 +9,7 @@ tmux start-server
 tmux new-session -d -s $session
 
 tmux selectp -t 0
-tmux send "cd ~/de-sandbox && [[ ! -z $(ifconfig | grep 172.16) ]] && vagrant up && vagrant ssh || echo 'Vagrant not started while VPN up'" C-m
+tmux send "cd ~/de-sandbox && [[ -z '$vpn_up' ]] && vagrant up && vagrant ssh || echo 'Vagrant not started while VPN up'" C-m
 tmux splitw -v -p 42
 
 tmux selectp -t 1
